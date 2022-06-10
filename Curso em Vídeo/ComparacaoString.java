122c791865d1a0aefb784e06687bb9f85b24a09b
Valmir <vmcfilho@gmail.com>
2022-06-10 17:43:00 -0300

    Update ComparacaoString.java

--- Curso em Vídeo/ComparacaoString.java
+++ Curso em Vídeo/ComparacaoString.java
@@ -6,9 +6,9 @@ public class ComparacaoString {
         String nome2 = "Gustavo";
         String nome3 = new String("Gustavo");
         String res, res1, res2;
-        res = (nome1==nome2)?"igual":"diferente";
+        res = (nome1 == nome2)?"igual":"diferente";
         System.out.println(res);
-        res1 = (nome1==nome3)?"igual":"diferente";
+        res1 = (nome1 == nome3)?"igual":"diferente";
         /*
         Mesmo o conteúdo sendo igual nas 3 variáveis nome,
         o resultado deu diferente, pois como o nome3 foi instanciado
@@ -16,7 +16,7 @@ public class ComparacaoString {
         estrutura. Para resolver isso fazemos o seguinte:
          */
         System.out.println(res1);
-        res2 = (nome1.equals(nome3))?"igual":"diferente"; // O "equals" verifica se o conteúdo é o mesmo.
+        res2 = (nome1.equals(nome3)) ? "igual" : "diferente"; // O "equals" verifica se o conteúdo é o mesmo.
         System.out.println(res2);
     }
 }
