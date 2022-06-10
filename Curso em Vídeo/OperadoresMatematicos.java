fe4d79c5b9614c7a84b8884a87644d6a5c592b49
Valmir <vmcfilho@gmail.com>
2022-06-10 17:46:47 -0300

    Update OperadoresMatematicos.java

--- Curso em Vídeo/OperadoresMatematicos.java
+++ Curso em Vídeo/OperadoresMatematicos.java
@@ -4,7 +4,7 @@ public class OperadoresMatematicos {
 
         int n1 = 8;
         int n2 = 8;
-        float m = (n1+n2) >> 1;
+        float m = (n1 + n2) >> 1;
         System.out.println("A média entre os 2 números é: "+ m);
     }
 }
