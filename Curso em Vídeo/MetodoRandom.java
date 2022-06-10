436f0e28675a92af94abd32d7ec77a2eec7d4e99
Valmir <vmcfilho@gmail.com>
2022-06-10 17:45:07 -0300

    Update MetodoRandom.java

--- Curso em Vídeo/MetodoRandom.java
+++ Curso em Vídeo/MetodoRandom.java
@@ -4,7 +4,7 @@ public class MetodoRandom {
 
         double aleatorio = Math.random(); // Gera um aleatório entre 0 e 1.
         System.out.println(aleatorio);
-        int n = (int) (5+aleatorio*(10-5)); // Gera um aleatório entre 5 e 10.
+        int n = (int) (5 + aleatorio * (10 - 5)); // Gera um aleatório entre 5 e 10.
         System.out.println(n);
     }
 }
