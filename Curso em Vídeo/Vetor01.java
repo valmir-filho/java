572d7828bb09338409c2b6b5a960070fb6706e56
Valmir <vmcfilho@gmail.com>
2022-06-10 17:49:41 -0300

    Update Vetor01.java

--- Curso em Vídeo/Vetor01.java
+++ Curso em Vídeo/Vetor01.java
@@ -4,7 +4,7 @@ public class Vetor01 {
 
         int[] n = {3,2,8,7,5,4};
         System.out.println(n.length); // Imprime o tamanho do vetor.
-        for(int c=0; c<=5; c++) {
+        for(int c = 0; c <= 5; c++) {
             System.out.println("Na posição " + c + " temos o valor "+ n[c]);
         }
     }
