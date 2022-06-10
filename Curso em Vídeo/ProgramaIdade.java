f2fc1dcfbda4ea00fcc1cbe8d52a5572a99b528b
Valmir <vmcfilho@gmail.com>
2022-06-10 17:47:24 -0300

    Update ProgramaIdade.java

--- Curso em Vídeo/ProgramaIdade.java
+++ Curso em Vídeo/ProgramaIdade.java
@@ -7,9 +7,9 @@ public class ProgramaIdade {
         Scanner t = new Scanner(System.in);
         System.out.print("Em que ano você nasceu? ");
         int nasc = t.nextInt();
-        int i = 2022-nasc;
+        int i = 2022 - nasc;
         System.out.println("Sua idade é " + i);
-        if (i>=18) {
+        if (i >= 18) {
             System.out.println("Maior de idade");
         } else {
             System.out.println("Menor de idade");
