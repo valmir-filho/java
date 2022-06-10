68754f85f13cec8f5b2cae9c9004221b0235c163
Valmir <vmcfilho@gmail.com>
2022-06-10 17:47:47 -0300

    Update ProgramaMedia.java

--- Curso em Vídeo/ProgramaMedia.java
+++ Curso em Vídeo/ProgramaMedia.java
@@ -9,9 +9,9 @@ public class ProgramaMedia {
         float n1 = teclado.nextFloat();
         System.out.print("Segunda nota: ");
         float n2 = teclado.nextFloat();
-        float m = (n1+n2)/2;
+        float m = (n1 + n2) / 2;
         System.out.println("Sua média foi " + m);
-        if (m>9) {
+        if (m > 9) {
             System.out.println("Parabéns!");
         }
     }
