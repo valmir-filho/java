c09c7f995c5ff2999fdb3a23a85c8233d6f4823f
Valmir <vmcfilho@gmail.com>
2022-06-10 17:50:02 -0300

    Update Vetor02.java

--- Curso em Vídeo/Vetor02.java
+++ Curso em Vídeo/Vetor02.java
@@ -4,7 +4,7 @@ public class Vetor02 {
 
         String[] mes = {"Jan","Fev","Mar","Abr","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez"};
         int[] tot = {31,28,31,30,31,30,31,31,30,31,30,31};
-        for(int c=0; c<mes.length; c++) {
+        for(int c = 0; c < mes.length; c++) {
             System.out.println("O mês de " + mes[c] + " tem " + tot[c] + " dias no total.");
         }
     }
