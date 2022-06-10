aec33d8ae3b8db25daf0c73c2cc54b6359dc99ee
Valmir <vmcfilho@gmail.com>
2022-06-10 17:48:35 -0300

    Update RepeticaoFor.java

--- Curso em Vídeo/RepeticaoFor.java
+++ Curso em Vídeo/RepeticaoFor.java
@@ -2,7 +2,7 @@ public class RepeticaoFor {
 
     public static void main(String[] args) {
 
-        for (int c=1; c<=100; c+=10) {
+        for (int c = 1; c <= 100; c += 10) {
             System.out.println(c);
         }
     }
