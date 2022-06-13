d850a52ba008d948604c05642464d204c3ac7813
Valmir <vmcfilho@gmail.com>
2022-06-13 18:27:19 -0300

    Add files via upload

---
+++ Udemy/estruturas-controle/If.java
@@ -0,0 +1,19 @@
+package estruturas.controle;
+
+import java.util.Scanner;
+
+public class If {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Digite a média do aluno: ");
+		double media = scan.nextDouble();
+		if(media >= 7.0) {
+			System.out.println("Aluno aprovado!");
+		} else {
+			System.out.println("Aluno reprovado!");
+		}
+		scan.close();
+	}
+}
