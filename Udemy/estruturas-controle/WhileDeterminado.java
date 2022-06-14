9d8c8996cae2e664e2f68bf92eb5bb106c113beb
Valmir <vmcfilho@gmail.com>
2022-06-14 12:12:01 -0300

    Add files via upload

---
+++ Udemy/estruturas-controle/WhileDeterminado.java
@@ -0,0 +1,13 @@
+package estruturas.controle;
+
+public class WhileDeterminado {
+	
+	public static void main(String[] args) {
+		
+		int contador = 1;
+		while(contador <= 10) {
+			System.out.printf("i = %d\n", contador);
+			contador++;
+		}
+	}
+}
