5c49fc862e39f6fada2b8c0b79a75664db6b7177
Valmir <vmcfilho@gmail.com>
2022-06-15 12:27:51 -0300

    Add files via upload

---
+++ Udemy/estruturas-controle/MaiorNumero.java
@@ -0,0 +1,23 @@
+package estruturas.controle;
+
+import java.util.Scanner;
+
+public class MaiorNumero {
+	
+	public static void main(String[] args) {
+		
+		int maiorValor = 0;
+		int contador = 0;
+		Scanner scan = new Scanner(System.in);
+		do {
+			System.out.print("Digite um número inteiro qualquer: ");
+			int valor = scan.nextInt();
+			if (valor > maiorValor) {
+				maiorValor = valor;
+			}
+			contador++;
+		} while (contador != 10);
+		System.out.printf("O maior número digitado foi o " + maiorValor + ".");
+		scan.close();
+	}
+}
