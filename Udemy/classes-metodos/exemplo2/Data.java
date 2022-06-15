aab5110cf3857c92524c471bcf215ccb5b354421
Valmir <vmcfilho@gmail.com>
2022-06-15 16:33:03 -0300

    Add files via upload

---
+++ Udemy/classes-metodos/exemplo2/Data.java
@@ -0,0 +1,11 @@
+package classes.metodos.exemplo2;
+
+public class Data {
+	
+	int dia, ano;
+	String mes;
+	
+	String dataFormatada() {
+		return String.format("%d de %s de %d.", dia, mes, ano);
+	}
+}
