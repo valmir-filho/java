bcbfdbfa7e67dc4d6235349694eac31b201f27e0
Valmir <vmcfilho@gmail.com>
2022-06-16 10:19:20 -0300

    Add files via upload

---
+++ Udemy/classes-metodos/exemplo9/Data.java
@@ -0,0 +1,31 @@
+package classes.metodos.exemplo9;
+
+public class Data {
+	
+	int dia, mes, ano;
+	
+	Data() {
+		
+		// dia = 1;
+		// mes = 1;
+		// ano = 1970;
+		this(1, 1, 1970);  // Aqui eu usei o método this();.
+	}
+	
+	Data(int dia, int mes, int ano) {
+		
+		this.dia = dia;  // Uso do this para referenciar o objeto atual.
+		this.mes = mes;
+		this.ano = ano;
+	}
+	
+	String dataFormatada() {
+		
+		return String.format("%d/%d/%d.", dia, mes, ano);
+	}
+	
+	void imprimirDataFormatada() {
+		
+		System.out.println(dataFormatada());
+	}
+}
