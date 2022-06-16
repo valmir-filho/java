5ba86736cca23b369ca71a28e8484b82d5d8efd0
Valmir <vmcfilho@gmail.com>
2022-06-16 11:54:50 -0300

    Add files via upload

---
+++ Udemy/classes-metodos/exemplo11/Comida.java
@@ -0,0 +1,13 @@
+package classes.metodos.exemplo11;
+
+public class Comida {
+	
+	String nomeComida;
+	double pesoComida;
+	
+	Comida(String nomeComida, double pesoComida) {
+		
+		this.nomeComida = nomeComida;
+		this.pesoComida = pesoComida;
+	}
+}
