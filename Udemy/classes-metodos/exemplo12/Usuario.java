f378eaf3e744c949b29906c57476f5a141b357c1
Valmir <vmcfilho@gmail.com>
2022-06-19 18:09:32 -0300

    Update Usuario.java

--- Udemy/classes-metodos/exemplo12/Usuario.java
+++ Udemy/classes-metodos/exemplo12/Usuario.java
@@ -1,15 +1,13 @@
 package classes.metodos.exemplo12;
 
 public class Usuario {
-	
 	String nome, email;
-	/*public boolean equals(Object objeto) {
-		
+	/* public boolean equals(Object objeto) {
 		Usuario outro = (Usuario) objeto;
 		boolean nomeIgual = outro.nome.equals(this.nome);
 		boolean emailIgual = outro.email.equals(this.email);
 		return nomeIgual && emailIgual;
 	}
-	Implementando dessa forma, daí o resultado do equals na classe
-	Usuario passa a ser verdadeiro.*/
+	Implementando dessa forma, daí o resultado do equals na classe Usuario passa a ser
+	verdadeiro. */
 }
