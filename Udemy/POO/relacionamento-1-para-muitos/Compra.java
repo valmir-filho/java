78dc975f9ef6b6aca695378f081b990ebeb6013b
Valmir <vmcfilho@gmail.com>
2022-06-19 18:21:10 -0300

    Update Compra.java

--- Udemy/POO/relacionamento-1-para-muitos/Compra.java
+++ Udemy/POO/relacionamento-1-para-muitos/Compra.java
@@ -3,10 +3,8 @@ package poo.relacionamento1paramuitos;
 import java.util.ArrayList;
 
 public class Compra {
-	
 	String cliente;
 	ArrayList<Item> itens = new ArrayList<Item>();
-	
 	double valorTotal() {	
 		double total = 0;
 		for(Item item: itens) {
