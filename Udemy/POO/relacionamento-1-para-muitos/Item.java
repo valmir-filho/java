bee1125f7d2542fdbc39d2d6a60c6bc811bcd111
Valmir <vmcfilho@gmail.com>
2022-06-19 18:21:38 -0300

    Update Item.java

--- Udemy/POO/relacionamento-1-para-muitos/Item.java
+++ Udemy/POO/relacionamento-1-para-muitos/Item.java
@@ -1,13 +1,10 @@
 package poo.relacionamento1paramuitos;
 
 public class Item {
-	
 	String nome;
 	int quantidade;
 	double preço;
-	
 	public Item(String nome, int quantidade, double preco) {
-		
 		this.nome = nome;
 		this.quantidade = quantidade;
 		this.preço = preco;
