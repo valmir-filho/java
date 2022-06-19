6ed8c2c267cc45a709a6f78e6c16274434bc9f7a
Valmir <vmcfilho@gmail.com>
2022-06-19 18:20:23 -0300

    Update Carro.java

--- Udemy/POO/relacionamento-1-para-1/Carro.java
+++ Udemy/POO/relacionamento-1-para-1/Carro.java
@@ -1,36 +1,24 @@
 package poo.relacionamento1para1;
 
 public class Carro {
-	
 	Motor motor = new Motor();
-	
 	void acelerar() {
-	
 		if(motor.fatorInjecao < 2.6) {
 			motor.fatorInjecao += 0.4;
 		}
 	}
-	
 	void frear() {
-		
 		if(motor.fatorInjecao > 0.5) {
-		
 			motor.fatorInjecao -= 0.4;
 		}
 	}
-	
 	void ligar() {
-		
 		motor.ligado = true;
 	}
-	
 	void desligar() {
-		
 		motor.ligado = false;
 	}
-	
 	boolean estaLigado() {
-		
 		return motor.ligado;
 	}
 }
