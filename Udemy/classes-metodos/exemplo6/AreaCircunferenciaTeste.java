7b2cf2cd60fc7bfb95bc094b407257a2f0ce48d4
Valmir <vmcfilho@gmail.com>
2022-06-19 18:15:00 -0300

    Update AreaCircunferenciaTeste.java

--- Udemy/classes-metodos/exemplo6/AreaCircunferenciaTeste.java
+++ Udemy/classes-metodos/exemplo6/AreaCircunferenciaTeste.java
@@ -1,9 +1,7 @@
 package classes.metodos.exemplo6;
 
 public class AreaCircunferenciaTeste {
-	
 	public static void main(String[] args) {
-		
 		AreaCircunferencia a1 = new AreaCircunferencia(10);
 		System.out.println(a1.area());
 		AreaCircunferencia a2 = new AreaCircunferencia(5);
