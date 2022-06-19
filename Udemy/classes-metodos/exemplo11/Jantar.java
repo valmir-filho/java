c7f5c2e24c278786c3cd4395e6de5f0c6f9b1cce
Valmir <vmcfilho@gmail.com>
2022-06-19 18:06:52 -0300

    Update Jantar.java

--- Udemy/classes-metodos/exemplo11/Jantar.java
+++ Udemy/classes-metodos/exemplo11/Jantar.java
@@ -1,9 +1,7 @@
 package classes.metodos.exemplo11;
 
 public class Jantar {
-	
 	public static void main(String[] args) {
-		
 		Pessoa p = new Pessoa("João da Silva", 73.4);
 		Comida c1 = new Comida("Feijão", 0.350);
 		Comida c2 = new Comida("Arroz", 0.450);
