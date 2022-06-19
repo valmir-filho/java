ce2181c6c4046caa666adb5ad2c9b072dacade6e
Valmir <vmcfilho@gmail.com>
2022-06-19 19:29:25 -0300

    Update Aluno.java

--- Udemy/POO/relacionamento-muito-para-muitos/Aluno.java
+++ Udemy/POO/relacionamento-muito-para-muitos/Aluno.java
@@ -5,8 +5,7 @@ import java.util.ArrayList;
 public class Aluno {
 	final String nome;
 	final ArrayList<Curso> cursos = new ArrayList<Curso>();
-	/* A palavra reservada "final", transformou a lista em uma lista constante,
-	ou seja, o endereço de memória não muda. */
+	/* A palavra reservada "final", transformou a lista em uma lista constante, ou seja, o endereço de memória não muda. */
 	Aluno(String nome) {
 		this.nome = nome;
 	}
