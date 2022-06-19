360145e736a24e95314378ddaada41166e6a4688
Valmir <vmcfilho@gmail.com>
2022-06-19 18:15:20 -0300

    Update Produto.java

--- Udemy/classes-metodos/exemplo7/Produto.java
+++ Udemy/classes-metodos/exemplo7/Produto.java
@@ -1,16 +1,12 @@
 package classes.metodos.exemplo7;
 
 public class Produto {
-	
 	String nome;
 	double preco;
 	static double desconto = 0.25;
-	
-	Produto(String nomeInicial) {
-		
+	Produto(String nomeInicial) {	
 		nome = nomeInicial;
 	}
-	
 	double precoDesconto() {
 		return preco * (1 - desconto);
 	}
