cfcbb793483eeba8172069deecca708bdf6372b3
Valmir <vmcfilho@gmail.com>
2022-06-19 18:14:45 -0300

    Update AreaCircunferencia.java

--- Udemy/classes-metodos/exemplo6/AreaCircunferencia.java
+++ Udemy/classes-metodos/exemplo6/AreaCircunferencia.java
@@ -1,23 +1,15 @@
 package classes.metodos.exemplo6;
 
 public class AreaCircunferencia {
-	
 	double raio;
 	static double pi = 3.14;
-	/*
-	 * Usando o modificador "static" eu transformei a 
-	 * variável (ou constante, ou método) que pertencia a instância
-	 * (ou ao objeto) para uma variável que pertence a classe.
-	 */
-	
+	/* Usando o modificador "static", transforma-se a variável (ou constante, ou método)
+	que pertencia a instância (ou ao objeto) para uma variável que pertence a classe. */
 	AreaCircunferencia(double raioInicial) {
-		
 		pi = 3.14;
 		raio = raioInicial;
 	}
-	
-	double area() {
-		
+	double area() {	
 		return pi * Math.pow(raio, 2);
 	}
 }
