da20b7bb321bcb50c94ffed9009c8384672dde5a
Valmir <vmcfilho@gmail.com>
2022-06-19 18:10:38 -0300

    Update Produto.java

--- Udemy/classes-metodos/exemplo3/Produto.java
+++ Udemy/classes-metodos/exemplo3/Produto.java
@@ -1,15 +1,11 @@
 package classes.metodos.exemplo3;
 
 public class Produto {
-	
 	String nome;
 	double preco, desconto;
-	
-	public Produto(String nomeInicial) {  // Criei o método construtor.
-		
+	public Produto(String nomeInicial) {  // Criado o método construtor.
 		nome = nomeInicial;
 	}
-	
 	double precoDesconto() {
 		return preco * (1 - desconto);
 	}
