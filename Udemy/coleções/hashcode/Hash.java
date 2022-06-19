ff931db46f29becfbbc07df11a040dec091d68d4
Valmir <vmcfilho@gmail.com>
2022-06-19 11:58:03 -0300

    Add files via upload

---
+++ Udemy/coleções/hashcode/Hash.java
@@ -0,0 +1,15 @@
+package colecoes;
+
+import java.util.HashSet;
+
+public class Hash {
+	
+	public static void main(String[] args) {
+		
+		HashSet<Usuario> usuarios = new HashSet<Usuario>();
+		usuarios.add(new Usuario("Pedro"));
+		usuarios.add(new Usuario("Ana"));
+		usuarios.add(new Usuario("João"));
+		System.out.println(usuarios.contains(new Usuario("João")));	
+	}
+}
