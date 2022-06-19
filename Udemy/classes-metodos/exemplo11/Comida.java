e6274ada4ba748946ead3116f0c01dc824eee575
Valmir <vmcfilho@gmail.com>
2022-06-19 18:06:38 -0300

    Update Comida.java

--- Udemy/classes-metodos/exemplo11/Comida.java
+++ Udemy/classes-metodos/exemplo11/Comida.java
@@ -1,12 +1,9 @@
 package classes.metodos.exemplo11;
 
 public class Comida {
-	
 	String nomeComida;
 	double pesoComida;
-	
 	Comida(String nomeComida, double pesoComida) {
-		
 		this.nomeComida = nomeComida;
 		this.pesoComida = pesoComida;
 	}
