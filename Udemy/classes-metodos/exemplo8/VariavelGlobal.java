6cd6aeccbfefe9aeb51973e9746ab4398828713c
Valmir <vmcfilho@gmail.com>
2022-06-19 18:16:37 -0300

    Update VariavelGlobal.java

--- Udemy/classes-metodos/exemplo8/VariavelGlobal.java
+++ Udemy/classes-metodos/exemplo8/VariavelGlobal.java
@@ -1,21 +1,13 @@
 package classes.metodos.exemplo8;
 
 public class VariavelGlobal {
-	
 	int a = 3;
-	/*
-	 * Poderia resolver também, sem precisar criar
-	 * um objeto, colocando a palavra reservada
-	 * static na frente: static int a = 3;.
-	 */
-	
+	/* Pode-se resolver também, sem precisar criar um objeto, colocando a palavra
+	reservada "static" na frente: static int a = 3;. */	
 	public static void main(String[] a) {	
-		
 		VariavelGlobal v = new VariavelGlobal();
-		/*
-		 * Para usar a variável global dentro desse método,
-		 * foi necessário criar um objeto.
-		 */
+		/* Para usar a variável global dentro desse método, foi necessário criar um
+		objeto. */
 		System.out.println(v.a);
 	}
 }
