db8f77aad4c61fef825230e01933648ae8388090
Valmir <vmcfilho@gmail.com>
2022-06-19 18:09:58 -0300

    Update Data.java

--- Udemy/classes-metodos/exemplo2/Data.java
+++ Udemy/classes-metodos/exemplo2/Data.java
@@ -1,10 +1,8 @@
 package classes.metodos.exemplo2;
 
 public class Data {
-	
 	int dia, ano;
 	String mes;
-	
 	String dataFormatada() {
 		return String.format("%d de %s de %d.", dia, mes, ano);
 	}
