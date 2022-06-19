2d928494783a17a361830bf37cdb5f66caf000ff
Valmir <vmcfilho@gmail.com>
2022-06-19 18:07:15 -0300

    Update Pessoa.java

--- Udemy/classes-metodos/exemplo11/Pessoa.java
+++ Udemy/classes-metodos/exemplo11/Pessoa.java
@@ -1,21 +1,17 @@
 package classes.metodos.exemplo11;
 
 public class Pessoa {
-	
 	String nomePessoa;
 	double pesoPessoa;
-	
 	Pessoa(String nomePessoa, double pesoComida) {
 		this.nomePessoa = nomePessoa;
 		this.pesoPessoa = pesoComida;
 	}
-	
 	void comer(Comida comida) {
 		if(comida != null) {
 			this.pesoPessoa += comida.pesoComida;
 		}
 	}
-	
 	String apresentar() {
 		return "Meu nome é " + nomePessoa + " e tenho " + pesoPessoa + " quilos.";
 	}
