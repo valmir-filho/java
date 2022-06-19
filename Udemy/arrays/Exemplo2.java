d82c55603ce049255f5bbe1a61c15a022b2c28c5
Valmir <vmcfilho@gmail.com>
2022-06-19 18:19:04 -0300

    Update Exemplo2.java

--- Udemy/arrays/Exemplo2.java
+++ Udemy/arrays/Exemplo2.java
@@ -3,9 +3,7 @@ package arrays;
 import java.util.Arrays;
 
 public class Exemplo2 {
-	
 	public static void main(String[] args) {
-		
 		double[] notasAluno1 = {6.9, 8.9, 5.5, 6.6};  // Outra forma de criar um array.
 		System.out.println("Notas do aluno 1: "+ Arrays.toString(notasAluno1));
 		double media = 0;
