8a494d57bbf9334ffaaab8a0cda72911c6cbf5c2
Valmir <vmcfilho@gmail.com>
2022-06-19 18:21:25 -0300

    Update CompraTeste.java

--- Udemy/POO/relacionamento-1-para-muitos/CompraTeste.java
+++ Udemy/POO/relacionamento-1-para-muitos/CompraTeste.java
@@ -1,9 +1,7 @@
 package poo.relacionamento1paramuitos;
 
 public class CompraTeste {
-	
 	public static void main(String[] args) {
-		
 		Compra compra1 = new Compra();
 		compra1.cliente = "João Pedro";
 		compra1.itens.add(new Item("Caneta", 20, 7.45));
