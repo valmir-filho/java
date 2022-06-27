c07e31952ca62776cbb98eccf7c3cf8ba06cbe9d
Valmir <vmcfilho@gmail.com>
2022-06-27 09:36:27 -0300

    Add files via upload

---
+++ Udemy/POO/herança/exemplo5/Paulo.java
@@ -0,0 +1,11 @@
+package heranca.encapsulamento.casa1;
+
+public class Paulo {
+	void testeAcessos() {
+		Ana esposa = new Ana();
+		// System.out.println(esposa.segredo);  // Esse atributo não está disponível, pois ele está com o modificador de acesso "private".
+		System.out.println(esposa.facoDentroDeCasa);  // Esse atributo está disponível, pois mesmo ele estando com o modificador de acesso "default", ele está no mesmo pacote.
+		System.out.println(esposa.formaDeFalar);  // Esse atributo está disponível, pois mesmo ele estando com o modificador de acesso "protected", ele está no mesmo pacote.
+		System.out.println(esposa.todosSabem);  // Esse atributo está disponível, pois ele está com o modificador de acesso "public".
+	}
+}
