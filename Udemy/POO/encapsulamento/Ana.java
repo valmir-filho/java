1c608753d8f90e9bd6d1ce6d98afddbc5286523c
Valmir <vmcfilho@gmail.com>
2022-06-28 11:44:04 -0300

    Add files via upload

---
+++ Udemy/POO/encapsulamento/Ana.java
@@ -0,0 +1,9 @@
+package encapsulamento.casa1;
+
+public class Ana {
+	@SuppressWarnings("unused")
+	private String segredo = "...";
+	String facoDentroDeCasa = "...";  // Como não foi identificado o modificador de acesso, por padrão ele é "default".
+	protected String formaDeFalar = "...";
+	public String todosSabem = "...";
+}
