09def59ec51166942fd10c21f2eb9de3862ebb55
Valmir <vmcfilho@gmail.com>
2022-06-30 17:21:16 -0300

    Update Predicado.java

--- Udemy/POO/funçoes-lambdas/exemplo5/Predicado.java
+++ Udemy/POO/funçoes-lambdas/exemplo5/Predicado.java
@@ -5,7 +5,7 @@ import java.util.function.Predicate;  // Interface funcional.
 public class Predicado {
 	public static void main(String[] args) {
 		Predicate<Produto> isCaro = produto -> (produto.preco * (1 - produto.desconto)) >= 750;
-		Produto produto = new Produto("Notebook", 3893.89, 0.85);
+		Produto produto = new Produto("Notebook", 3893.89, 0.15);
 		System.out.println(isCaro.test(produto));
 	}
 }
