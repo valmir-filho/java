af2f249f83de841259c952e41b39089557addd62
Valmir <vmcfilho@gmail.com>
2022-07-01 17:21:53 -0300

    Add files via upload

---
+++ Udemy/POO/funçoes-lambdas/exemplo11/OperadorBinario.java
@@ -0,0 +1,19 @@
+package lambdas.exemplo11;
+
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+
+public class OperadorBinario {  // Interface funcional.
+	public static void main(String[] args) {
+		BinaryOperator<Double> media = (n1, n2) -> (n1 + n2) / 2;
+		System.out.println(media.apply(9.8, 5.7));
+		BiFunction<Double, Double, String> resultado = (n1, n2) -> {
+			double notaFinal = ((n1 + n2) / 2);
+			return notaFinal >= 7 ? "Aprovado" : "Reprovado";
+		};
+		System.out.println(resultado.apply(9.7, 4.1));
+		Function<Double, String> conceito = m -> m >= 7 ? "Aprovado" : "Reprovado";
+		System.out.println(media.andThen(conceito).apply(9.7, 4.1));
+	}
+}
