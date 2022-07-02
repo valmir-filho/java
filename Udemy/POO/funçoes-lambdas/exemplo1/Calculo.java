3c9255887f6679bc18ecf1fb4356d37c42e57a5a
Valmir <vmcfilho@gmail.com>
2022-07-02 14:55:45 -0300

    Delete Udemy/POO/funçoes-lambdas directory

--- Udemy/POO/funçoes-lambdas/exemplo1/Calculo.java
+++
@@ -1,5 +0,0 @@
-package lambdas.exemplo1;
-
-public interface Calculo {
-	public double executar(double a, double b);  // Em uma interface, os métodos são sempre públicos. 
-}
--- Udemy/POO/funçoes-lambdas/exemplo2/Calculo.java
+++
@@ -1,6 +0,0 @@
-package lambdas.exemplo2;
-
-@FunctionalInterface  // Interface funcional. Pode ter somente 1 único método.
-public interface Calculo {
-	public double executar(double a, double b);  // Mesmo não estando explícito, esse método é sempre abstrato.
-}
