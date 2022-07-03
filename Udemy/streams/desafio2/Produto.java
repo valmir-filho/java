0f1dcd4bffb951e0410e6bb3330e2755ab8b586f
Valmir <vmcfilho@gmail.com>
2022-07-03 18:26:40 -0300

    Add files via upload

---
+++ Udemy/streams/desafio2/Produto.java
@@ -0,0 +1,14 @@
+package streams.desafio2;
+
+public class Produto {
+	final String nome;
+	final double preco;
+	final double desconto;
+	final double valorFrete;
+	public Produto(String nome, double preco, double desconto, double valorFrete) {
+		this.nome = nome;
+		this.preco = preco;
+		this.desconto = desconto;
+		this.valorFrete = valorFrete;
+	}	
+}
