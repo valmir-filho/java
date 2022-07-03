dd809e46f61852f81dd8b51dfdef0a47d451742f
Valmir <vmcfilho@gmail.com>
2022-07-03 11:10:46 -0300

    Add files via upload

---
+++ Udemy/streams/exemplo4/Aluno.java
@@ -0,0 +1,10 @@
+package streams.exemplo4;
+
+public class Aluno {
+	final String nome;
+	final double nota;
+	public Aluno(String nome, double nota) {
+		this.nome = nome;
+		this.nota = nota;
+	}
+}
