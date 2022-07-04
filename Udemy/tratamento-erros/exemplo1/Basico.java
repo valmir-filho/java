9e9ab33ed869014b196a0b6a0b427767fbbb5341
Valmir <vmcfilho@gmail.com>
2022-07-04 14:33:36 -0300

    Update Basico.java

--- Udemy/tratamento-erros/exemplo1/Basico.java
+++ Udemy/tratamento-erros/exemplo1/Basico.java
@@ -8,17 +8,14 @@ public class Basico {
 		} catch(Exception excecao) {
 			System.out.println("Erro no momento de imprimir o nome do usuário.");
 		}
-		/* Sem o uso do "try/catch", aconteceria o seguinte erro: 
-		 * Exception in thread "main" java.lang.NullPointerException: Cannot
-		 * read field "nome" because "aluno" is null. */
+		/* Sem o uso do "try/catch", aconteceria o seguinte erro: Exception in thread "main" java.lang.NullPointerException: Cannot read
+		field "nome" because "aluno" is null. */
 		try {
 			System.out.println(7/0);
 		} catch (ArithmeticException excecao) {
 			System.out.println("Erro: " + excecao.getMessage());;
 		}
-		/* Sem o uso do "try/catch", aconteceria o seguinte erro:
-		 * Exception in thread "main" java.lang.ArithmeticException:
-		 * / by zero. */
+		// Sem o uso do "try/catch", aconteceria o seguinte erro: Exception in thread "main" java.lang.ArithmeticException: / by zero.
 		System.out.println("Fim!");
 	}
 	public static void imprimirNomeDoAluno(Aluno aluno) {
