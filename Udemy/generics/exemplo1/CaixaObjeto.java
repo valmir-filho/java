5e8a7700e1927d9af34e3c5688bfdb17a7ece2be
Valmir <vmcfilho@gmail.com>
2022-07-07 10:17:55 -0300

    Add files via upload

---
+++ Udemy/generics/exemplo1/CaixaObjeto.java
@@ -0,0 +1,11 @@
+package generics.exemplo1;
+
+public class CaixaObjeto {
+	private Object coisa;
+	public void guardar(Object coisa) {
+		this.coisa = coisa;
+	}
+	public Object abrir() {
+		return coisa;
+	}
+}
