71354662db1a031cc1136f3809e5d6d747dfc04d
Valmir <vmcfilho@gmail.com>
2022-07-07 10:51:44 -0300

    Add files via upload

---
+++ Udemy/generics/exemplo2/CaixaTeste.java
@@ -0,0 +1,14 @@
+package generics.exemplo2;
+
+public class CaixaTeste {
+	public static void main(String[] args) {
+		Caixa<String> caixaA = new Caixa<>();
+		caixaA.guardar("Segredo!");
+		String coisaA = caixaA.abrir();
+		System.out.println(coisaA);
+		Caixa<Double> caixaB = new Caixa<>();
+		caixaB.guardar(3.1415);
+		Double coisaB = caixaB.abrir();
+		System.out.println(coisaB);
+	}
+}
