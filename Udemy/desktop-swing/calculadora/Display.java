abc794456904d5b7f16d65de074569ea492de821
Valmir <vmcfilho@gmail.com>
2022-07-12 12:38:33 -0300

    Update Display.java

--- Udemy/desktop-swing/calculadora/Display.java
+++ Udemy/desktop-swing/calculadora/Display.java
@@ -14,7 +14,8 @@ public class Display extends JPanel implements MemoriaObservador {
 		setBackground(new Color(46, 49, 50));  // Comando para colorir o display (local onde aparecerão os números digitados na calculadora).
 		label = new JLabel(Memoria.getInstancia().getTextoAtual());
 		label.setForeground(Color.white);  // Comando para colorir o label (os números digitados que aparecerão no display da calculadora).
-		label.setFont(new Font("courier", Font.PLAIN, 30));  // Comando para definição da fonte (tipo, formato e tamanho) dos números digitados que aparecerão no display da calculadora.
+		label.setFont(new Font("courier", Font.PLAIN, 30));
+		// Comando para definição da fonte (tipo, formato, tamanho) dos números digitados que aparecerão no display da calculadora.
 		setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
 		/* Comando para definição do alinhamento dos números digitados que aparecerão no display da calculadora. Os valores após o
 		alinhamento a direita, definem o alinhamento acima e abaixo dos números digitados que aparecerão no display da calculadora. */
