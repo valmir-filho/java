e4a9ecbe683dc4d596d6af4927626e14c8483ae8
Valmir <vmcfilho@gmail.com>
2022-07-13 19:36:58 -0300

    Update FabricaConexao.java

--- Udemy/banco-dados-JDBC/FabricaConexao.java
+++ Udemy/banco-dados-JDBC/FabricaConexao.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class FabricaConexao {  // Classe para reutilização da criação de conexão.
+public class FabricaConexao {  // Classe para reutilização da criação de conexão com o BD.
 	public static Connection getConexao() {
 		try {
 			final String url = "jdbc:mysql://localhost/curso_java?verifyServerCertificate=false&useSSL=true";
