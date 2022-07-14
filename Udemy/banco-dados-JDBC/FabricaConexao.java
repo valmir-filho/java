c9f6b7ed669093d197d02bde8652e7bdf39af714
Valmir <vmcfilho@gmail.com>
2022-07-14 15:15:21 -0300

    Add files via upload

--- Udemy/banco-dados-JDBC/FabricaConexao.java
+++ Udemy/banco-dados-JDBC/FabricaConexao.java
@@ -1,18 +1,28 @@
 package jdbc;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class FabricaConexao {  // Classe para reutilização da criação de conexão com o BD.
+	// Método para externalização de dados sensíveis, utilizando o "Properties".
 	public static Connection getConexao() {
 		try {
-			final String url = "jdbc:mysql://localhost/curso_java?verifyServerCertificate=false&useSSL=true";
-			final String usuario = "root";
-			final String senha = "val25790";
+			Properties prop = getProperties();
+			final String url = prop.getProperty("banco.url");
+			final String usuario = prop.getProperty("banco.usuario");
+			final String senha = prop.getProperty("banco.senha");
 			return DriverManager.getConnection(url, usuario, senha);
-		} catch (SQLException e) {
+		} catch (SQLException | IOException e) {
 			throw new RuntimeException(e);
 		}
+	}
+	private static Properties getProperties() throws IOException {
+		Properties prop = new Properties();
+		String caminho = "/conexao.properties";
+		prop.load(FabricaConexao.class.getResourceAsStream(caminho));
+		return prop;
 	}
 }
