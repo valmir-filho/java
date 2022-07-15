c923eafa831e6e5c69718207617a59098036d461
Valmir <vmcfilho@gmail.com>
2022-07-15 12:09:49 -0300

    Add files via upload

--- Udemy/JPA/AlterarUsuario1.java
+++ Udemy/JPA/AlterarUsuario1.java
@@ -5,15 +5,16 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import modelo.basico.Usuario;
 
-public class NovoUsuario {
+public class AlterarUsuario1 {
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
 		EntityManager em = emf.createEntityManager();
-		Usuario novoUsuario = new Usuario("Leonardo", "leonardo@lanche.com.br");
 		em.getTransaction().begin();
-		em.persist(novoUsuario);  // Método para inserção no BD "curso_java".
+		Usuario usuario = em.find(Usuario.class, 7L);
+		usuario.setNome("Leo");  // Método para informar a alteração do dado (nesse caso o "nome"), na tabela "Usuario" do BD "curso_java".
+		usuario.setEmail("leo@lanche.com.br");
+		em.merge(usuario);  // Método para a alteração dos dados na tabela "Usuario" do BD "curso_java".
 		em.getTransaction().commit();
-		System.out.println("O id gerado foi: " + novoUsuario.getId());
 		em.close();
 		emf.close();
 	}
--- Udemy/JPA/AlterarUsuario2.java
+++ Udemy/JPA/AlterarUsuario2.java
@@ -5,15 +5,17 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import modelo.basico.Usuario;
 
-public class NovoUsuario {
+public class AlterarUsuario2 {
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
 		EntityManager em = emf.createEntityManager();
-		Usuario novoUsuario = new Usuario("Leonardo", "leonardo@lanche.com.br");
 		em.getTransaction().begin();
-		em.persist(novoUsuario);  // Método para inserção no BD "curso_java".
+		Usuario usuario = em.find(Usuario.class, 7L);
+		em.detach(usuario);  // Método para desassociar o usuário do estado gerenciado.
+		usuario.setNome("Leonardo");
+		usuario.setEmail("leonardo@lanche.com.br");
+		em.merge(usuario);
 		em.getTransaction().commit();
-		System.out.println("O id gerado foi: " + novoUsuario.getId());
 		em.close();
 		emf.close();
 	}
