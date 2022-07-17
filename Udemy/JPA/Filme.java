10a625b0e12543d40da5418d0f165f7e14f755d5
Valmir <vmcfilho@gmail.com>
2022-07-17 18:40:29 -0300

    Update Filme.java

--- Udemy/JPA/Filme.java
+++ Udemy/JPA/Filme.java
@@ -2,7 +2,6 @@ package modelo.muitosparamuitos;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
