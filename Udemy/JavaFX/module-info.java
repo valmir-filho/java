47a61ea189e0230039bc285f6d49c31d22e5dcd7
Valmir <vmcfilho@gmail.com>
2022-08-20 23:17:53 -0300

    Add files via upload

--- Udemy/JavaFX/module-info.java
+++ Udemy/JavaFX/module-info.java
@@ -1,5 +1,9 @@
 module exerciciosfx {
 	requires javafx.controls;
 	requires javafx.graphics;
+	requires javafx.fxml;
+	requires org.controlsfx.controls;
 	opens basico;
+	opens layout;
+	opens fxml;
 }
