22953366f8bfa699347758f8cd375a0b36be50c0
Valmir <vmcfilho@gmail.com>
2022-08-25 10:11:39 -0300

    Add files via upload

--- Udemy/spring-boot/ClienteController.java
+++ Udemy/spring-boot/ClienteController.java
@@ -1,6 +1,7 @@
 package exercicios.spring.boot.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,9 @@ public class ClienteController {
 	@GetMapping(path = "/qualquer")
 	public Cliente obterCliente() {
 		return new Cliente(28, "Pedro", "123.456.789-00");
+	}
+	@GetMapping("/{id}")
+	public Cliente obterClientePorId(@PathVariable int id) {
+		return new Cliente(id, "Maria", "987.654.321-00");
 	}
 }
