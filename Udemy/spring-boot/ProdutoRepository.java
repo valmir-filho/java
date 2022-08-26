02b139868b410ab7d68f111f62b1461076774570
Valmir <vmcfilho@gmail.com>
2022-08-26 11:36:56 -0300

    Add files via upload

--- Udemy/spring-boot/ProdutoRepository.java
+++ Udemy/spring-boot/ProdutoRepository.java
@@ -1,7 +1,8 @@
 package exercicios.spring.boot.model.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import exercicios.spring.boot.model.entities.Produto;
 
-public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer> {
+	public Iterable<Produto> findByNomeContainingIgnoreCase(String parteNome);
 }
