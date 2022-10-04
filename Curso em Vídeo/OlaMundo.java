cde801387ffed493f5b4b6dd86c3fef538d6829f
Valmir <vmcfilho@gmail.com>
2022-10-04 13:57:14 -0300

    Delete Curso em Vídeo directory

--- Curso em Vídeo/OlaMundo.java
+++
@@ -1,6 +0,0 @@
-public class OlaMundo {
-
-    public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
-    }
-}
--- Curso em Vídeo/RepeticaoFor.java
+++
@@ -1,9 +0,0 @@
-public class RepeticaoFor {
-
-    public static void main(String[] args) {
-
-        for (int c = 1; c <= 100; c += 10) {
-            System.out.println(c);
-        }
-    }
-}
