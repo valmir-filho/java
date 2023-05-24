00224baf05d0d0e217bd6ccdd030925125b2a037
Valmir <vmcfilho@gmail.com>
2023-05-24 18:11:22 -0300

    Update Informacao1.java

--- PUCPR/ads/2022/fundamentos-big-data/map-reduce/classes/Informacao1.java
+++ PUCPR/ads/2022/fundamentos-big-data/map-reduce/classes/Informacao1.java
@@ -26,39 +26,58 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class Informacao1 {
     
     public static class MapperInformacao1 extends Mapper<Object, Text, Text, IntWritable> {
+        private final static IntWritable ocorrencia = new IntWritable(1);
+        private final Text chaveMap = new Text();
+        
         @Override
         public void map(Object chave, Text valor, Context context) throws IOException, InterruptedException {
             String linha = valor.toString();
             String[] campos = linha.split(";");
-            if(campos.length == 10) {
-                String pais = campos[0];
-                int ocorrencia = 1;
-                Text chaveMap = new Text(pais);
-                IntWritable valorMap = new IntWritable(ocorrencia);
-                context.write(chaveMap, valorMap);
+            
+            if (campos.length == 10) {
+                String pais = campos[0].trim();
+                chaveMap.set(pais);
+                context.write(chaveMap, ocorrencia);
             }
         }
     }
     
     public static class ReducerInformacao1 extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private final Text paisMax = new Text();
+        private final IntWritable maxOcorrencia = new IntWritable(0);
+        
         @Override
         public void reduce(Text chave, Iterable<IntWritable> valores, Context context) throws IOException, InterruptedException {
             int soma = 0;
+            
             for (IntWritable val : valores) {
                 soma += val.get();
             }
-            IntWritable valorSaida = new IntWritable(soma);
-            context.write(chave, valorSaida);
+            
+            if (soma > maxOcorrencia.get()) {
+                maxOcorrencia.set(soma);
+                paisMax.set(chave);
+            }
+            
+            context.write(chave, new IntWritable(soma));
+        }
+        
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+            context.write(new Text("País com a maior quantidade de transações:"), maxOcorrencia);
+            context.write(paisMax, maxOcorrencia);
         }
     }
     
     public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
         String arquivoEntrada = "/Users/valmirfilho/NetBeansProjects/implementacaoMapReduce/src/main/java/mycompany/implementacaomapreduce/base_100_mil.csv";
         String arquivoSaida = "/Users/valmirfilho/NetBeansProjects/implementacaoMapReduce/src/main/java/mycompany/implementacaomapreduce/informacao1";
+        
         if(args.length == 2) {
             arquivoEntrada = args[0];
             arquivoSaida = args[1];
         }
+        
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "informacao1");
         job.setJarByClass(Informacao1.class);
@@ -68,6 +87,6 @@ public class Informacao1 {
         job.setOutputValueClass(IntWritable.class);
         FileInputFormat.addInputPath(job, new Path(arquivoEntrada));
         FileOutputFormat.setOutputPath(job, new Path(arquivoSaida));
-        job.waitForCompletion(true);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
